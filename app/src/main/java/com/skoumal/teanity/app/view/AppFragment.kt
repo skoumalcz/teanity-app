bfa1a9adeb275f65e8ab69d31728b9629d8145e2
viktor de pasquale <info@depasquale.wiki>
2019-09-05 09:03:07 +0200

    Removed intermediate scroll container

--- app/src/main/java/com/skoumal/teanity/app/view/AppFragment.kt
+++ app/src/main/java/com/skoumal/teanity/app/view/AppFragment.kt
@@ -1,5 +1,9 @@
 package com.skoumal.teanity.app.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.LifecycleOwner
 import com.skoumal.teanity.app.model.navigation.Destinations
@@ -28,6 +32,16 @@ abstract class AppFragment<ViewModel : AppViewModel, Binding : ViewDataBinding> 
         when (event) {
             Destinations.Up -> navController.navigateUp()
         }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val superCall = super.onCreateView(inflater, container, savedInstanceState)
+        binding.executePendingBindings()
+        return superCall
     }
 
     protected inline fun <reified T : ViewModel> LifecycleOwner.viewModel(noinline parameters: ParametersDefinition? = null) =
