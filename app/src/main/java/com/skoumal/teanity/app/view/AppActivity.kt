a9fa7a3d57224a3f1652a3f1980d8bb31e2db20a
Viktor De Pasquale <info@depasquale.wiki>
2020-07-08 13:47:32 +0200

    Updated theme to allow for edge-to-edge experience

--- app/src/main/java/com/skoumal/teanity/app/view/AppActivity.kt
+++ app/src/main/java/com/skoumal/teanity/app/view/AppActivity.kt
@@ -1,7 +1,28 @@
 package com.skoumal.teanity.app.view
 
+import android.os.Bundle
 import androidx.databinding.ViewDataBinding
+import com.skoumal.teanity.app.R
+import com.skoumal.teanity.ui.extensions.translucency
+import com.skoumal.teanity.ui.extensions.transparency
 import com.skoumal.teanity.view.TeanityActivity
 
 abstract class AppActivity<ViewModel : AppViewModel, Binding : ViewDataBinding> :
-    TeanityActivity<ViewModel, Binding>()
+    TeanityActivity<ViewModel, Binding>() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme)
+        translucency {
+            isNavBarTranslucent = true
+            isStatusBarTranslucent = true
+        }
+        transparency {
+            isNavBarTransparent = true
+            isNavBarLight = !isDarkMode
+            isStatusBarTransparent = true
+            isStatusBarLight = !isDarkMode
+        }
+        super.onCreate(savedInstanceState)
+    }
+
+}
